290998d80c435bc0d5a22cb85ca9b2b37c3681b1
AwJoey <s10196528@connect.np.edu.sg>
2022-01-12 11:28:56 +0800

    added footer

---
+++ footer.php
@@ -0,0 +1,49 @@
+<link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet'>
+<link rel="stylesheet" href="css/bootstrap.css">
+<!--jQuery library-->
+<script src="js/jquery-3.3.1.min.js"></script>
+<!--Latest compiled Javascript-->
+<script src="js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="css/site.css">
+</div> <!-- close the column in 3rd row -->
+        </div> <!-- close the 3rd Row -->
+        <!-- 4th Row -->
+        <div style="background:#63200D;font-family: 'Quicksand'">
+            <div style="text-align:right">
+                <img src="Images/doughnut_white2.png" style="width:30%; padding-right:20px">
+            </div>
+            <div class="row">
+            <div class="col-sm-6" style="color:#FFFFFF">
+                <p>
+                    Tel: <a class="footerRefs" href="tel:+65 91234567">+65 91234567</a>
+</p>
+                <p>
+                    Email: <a class="footerRefs" href="mailto:hello@donut.com">hello@donut.com</a>
+</p>
+                <p>
+                    Address: 123 Donut Street 45 #06-12 S123456  
+</p>
+            </div>
+            <div class="col-sm-6" style="color:#FFFFFF">
+                <p>
+                    COPYRIGHT © 2022 ALL RIGHTS RESERVED BY HELLO DONUTS®
+</p>
+                <p>
+                    MADE BY MING ZHE, JOLENE, YUXUAN, JOEY 
+</p>
+            </div>
+</div>
+            
+        </div> <!-- Close the bootstrap container
+    </body>
+</html>
+        -->
+
+<style>
+    .footerContent {
+        
+    }
+    .footerRefs {
+        color:#FFFFFF;
+    }
+</style>
