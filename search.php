156281be73f210ffd8e260a390fc558d7013664f
jolenecpn <jolene.cheongg@gmail.com>
2022-02-06 01:12:32 +0800

    responsive search done

--- search.php
+++ search.php
@@ -40,7 +40,7 @@ if (isset($_GET["keywords"]) && trim($_GET['keywords']) != "") {
         $formattedPrice = number_format($row["Price"], 2);
 	    $formattedOffer = number_format($row["OfferedPrice"], 2);
         echo "<div class='col-8' >"; 
-        echo "<h4 style='margin-top: 110px; margin-left: 150px'><a style='color: #63200D;' href=$product>$row[ProductTitle]</a></h4>";
+        echo "<h4 class='here'style='margin-top: 110px; margin-left: 150px'><a style='color: #63200D;' href=$product>$row[ProductTitle]</a></h4>";
         if (( $row["Quantity"] >0 and $row["Quantity"] <= 100) && ($row["Offered"] == 1) and $row["OfferStartDate"] <= $date_now and $row["OfferEndDate"] >= $date_now ){
             echo "<div style='display: flex'>";
             echo "<div class='onsale' style='margin-left: 150px'>ON SALE</div>";
@@ -56,7 +56,7 @@ if (isset($_GET["keywords"]) && trim($_GET['keywords']) != "") {
         echo "</div>";
         $img = "./Images/products/$row[ProductImage]";
         echo "<div class='col-4' >";
-        echo "<a href=$product><img style='border-radius: 10%; width: 20em; height: 20em; object-fit: cover; margin-bottom: 30px;' src='$img' /></a>";
+        echo "<a href=$product><img class='help'style='border-radius: 10%; width: 20em; height: 20em; object-fit: cover; margin-bottom: 30px;' src='$img' /></a>";
         echo "</div>";
         echo "</div>"; 
     }
@@ -75,21 +75,26 @@ include("footer.php");
 			margin: 30px 0 0 10px !important;
         }
         .onsale{
+            margin-left:20px !important;
             width: 150px !important; 
 			font-size: 15px !important;
 			border-radius: 10px !important;
 			height: 25px !important;
         }
         .onsale2{
+            margin-left:10px !important;
             width: 150px !important; 
 			font-size: 15px !important;
 			border-radius: 10px !important;
 			height: 50px !important;
         }
-		.help{
+		.here{
+			margin-left:20px !important;
+		}
+        .help{
 			margin-top:100px;
-			width: 120px !important; 
-			height: 120px !important; 
+			width: 100px !important; 
+			height: 100px !important; 
 		}
       }
 </style>
